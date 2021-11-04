b84427db316c473fb03f109dc9cbec9416c7060e
Miran Hojnik <miran@3dot.net>
2021-11-04 12:56:05 +0100

    minor hls updates

--- scripts/hls.sh
+++ scripts/hls.sh
@@ -7,7 +7,7 @@ on_die ()
 
 trap 'on_die' TERM
 cd /opt/data/hls/;
-ffmpeg -i "rtmp://localhost:1935/$1/$2" \
+ffmpeg -i "rtmp://127.0.0.1:1935/$1/$2" \
     -filter_complex "[v:0]scale=1920:-2[vout1];[v:0]scale=1280:-2[vout2];[v:0]scale=320:-2[vout3]" \
     -g 25 -sc_threshold 0 \
     -map "[vout2]" -c:v:0 libx264 -b:v:0 1000k -maxrate:v:0 2500k -bufsize:v:0 5000k \
@@ -15,7 +15,7 @@ ffmpeg -i "rtmp://localhost:1935/$1/$2" \
     -map "[vout1]" -c:v:2 libx264 -b:v:2 2500k -maxrate:v:2 5000k -bufsize:v:2 8000k \
     -map a:0 -c:a aac -b:a 92k -ac 2 \
     -f hls -hls_time 6 -hls_segment_type fmp4 \
-    -master_pl_name master.m3u8 \
+    -master_pl_name live.m3u8 \
     -strftime 1 -hls_segment_filename output_%v/segment_%s.m4s \
-    -var_stream_map "a:0,agroup:audio v:0,agroup:audio v:1,agroup:audio v:2,agroup:audio" output_%v/stream.m3u8 &
+    -var_stream_map "a:0,agroup:audio v:0,agroup:audio v:1,agroup:audio v:2,agroup:audio" output_%v/live.m3u8 &
 wait
